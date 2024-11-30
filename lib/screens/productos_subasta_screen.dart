ab5756317f5b6a48d40e4983a5a3b109f988f3ec
ignammm <negrarokimanchi@gmail.com>
2024-11-30 18:59:19 -0300

    #New: buscador producto en listra de productos

--- lib/screens/productos_subasta_screen.dart
+++ lib/screens/productos_subasta_screen.dart
@@ -17,21 +17,41 @@ class _ProductosScreenState extends State<ProductosScreen> {
   final ApiService apiService = ApiService();
   late Future<List<dynamic>> productosFuture;
   late Future<dynamic> subastaFuture;
+  final TextEditingController searchController = TextEditingController();
+  List<dynamic> productos = [];
+  List<dynamic> productosFiltrados = [];
 
   @override
   void initState() {
     super.initState();
     productosFuture = apiService.fetchProductosSubastaId(widget.idSubasta);
     subastaFuture = apiService.fetchSubastaId(widget.idSubasta);
+
+    // Cargar productos al inicializar
+    productosFuture.then((data) {
+      setState(() {
+        productos = data
+            .where((producto) => producto['estadoSolicitud'] == 'Aprobada')
+            .toList();
+        productosFiltrados = List.from(productos); // Copiar la lista
+      });
+    });
+  }
+
+  void _filtrarProductos(String query) {
+    setState(() {
+      productosFiltrados = productos
+          .where((producto) =>
+              producto['nombre'].toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 
   Future<String> _calcularGanador(dynamic producto) async {
     try {
-      // Obtén los detalles de la subasta
       final subasta = await subastaFuture;
       DateTime fechaFin = DateTime.parse(subasta['fechaFin']);
 
-      // Verifica si la subasta ya terminó
       if (DateTime.now().isAfter(fechaFin)) {
         final ofertas = await apiService.fetchOfertasProductoId(producto['id']);
         if (ofertas.isNotEmpty) {
@@ -48,152 +68,132 @@ class _ProductosScreenState extends State<ProductosScreen> {
         return "${producto['nombre']}: ${producto['descripcion']}";
       }
     } catch (e) {
-      return "No recibio ninguna oferta.";
+      return "No recibió ninguna oferta.";
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // if (widget.producto['urls'] != null) {
-    //   try {
-    //     imageUrls = List<String>.from(json.decode(widget.producto['urls']));
-    //   } catch (e) {
-    //     print("Error decoding URLs: $e");
-    //   }
-    // }
-
-    // String imageUrl = imageUrls.isNotEmpty
-    //     ? "https://imagenesproducto.blob.core.windows.net/imagenes/${imageUrls[0]}"
-    //     : "https://via.placeholder.com/150";
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos de la Subasta'),
-      ),
-      body: FutureBuilder<List<dynamic>>(
-        future: productosFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-                child: Text('No hay productos en esta subasta'));
-          } else {
-            var productos = snapshot.data!
-                .where((producto) => producto['estadoSolicitud'] == 'Aprobada')
-                .toList();
-
-            if (productos.isEmpty) {
-              return const Center(
-                  child: Text('No hay productos aprobados en esta subasta'));
-            }
-
-            return ListView.builder(
-  itemCount: productos.length,
-  itemBuilder: (context, index) {
-    var producto = productos[index];
-
-    // Obtener la URL de la imagen del producto
-    List<String> imageUrls = [];
-    if (producto['urls'] != null) {
-      try {
-        imageUrls = List<String>.from(json.decode(producto['urls']));
-      } catch (e) {
-        print("Error decoding URLs: $e");
-      }
-    }
-
-    String imageUrl = imageUrls.isNotEmpty
-        ? "https://imagenesproducto.blob.core.windows.net/imagenes/${imageUrls[0]}"
-        : "https://via.placeholder.com/150";
-
-    // Variable para detectar si el ratón está encima
-    bool isHovered = false;
-
-    return StatefulBuilder(
-      builder: (context, setState) {
-        return Card(
-          child: MouseRegion(
-            onEnter: (_) => setState(() => isHovered = true),
-            onExit: (_) => setState(() => isHovered = false),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    // Imagen dentro de GestureDetector para detectar clics
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                ProductoDetalleScreen(producto: producto),
-                          ),
-                        );
-                      },
-                      child: Image.network(
-                        imageUrl,
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) {
-                          return const Icon(Icons.image_not_supported,
-                              size: 200);
-                        },
-                      ),
-                    ),
-                    // Capa de oscurecimiento
-                    if (isHovered)
-                      Positioned.fill(
-                        child: Container(
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                      ),
-                  ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              onChanged: _filtrarProductos,
+              decoration: InputDecoration(
+                hintText: 'Buscar producto...',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                // Contenido del ListTile
-                ListTile(
-                  title: Text(producto['nombre']),
-                  subtitle: FutureBuilder<String>(
-                    future: _calcularGanador(producto),
-                    builder: (context, ganadorSnapshot) {
-                      if (ganadorSnapshot.connectionState ==
-                          ConnectionState.waiting) {
-                        return const Text("Calculando...");
-                      } else if (ganadorSnapshot.hasError) {
-                        return Text("Error: ${ganadorSnapshot.error}");
-                      } else {
-                        return Text(ganadorSnapshot.data ?? '');
-                      }
-                    },
-                  ),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            ProductoDetalleScreen(producto: producto),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: productosFiltrados.isEmpty
+          ? const Center(child: Text('No hay productos que coincidan'))
+          : ListView.builder(
+              itemCount: productosFiltrados.length,
+              itemBuilder: (context, index) {
+                var producto = productosFiltrados[index];
+
+                List<String> imageUrls = [];
+                if (producto['urls'] != null) {
+                  try {
+                    imageUrls =
+                        List<String>.from(json.decode(producto['urls']));
+                  } catch (e) {
+                    print("Error decoding URLs: $e");
+                  }
+                }
+
+                String imageUrl = imageUrls.isNotEmpty
+                    ? "https://imagenesproducto.blob.core.windows.net/imagenes/${imageUrls[0]}"
+                    : "https://via.placeholder.com/150";
+
+                bool isHovered = false;
+
+                return StatefulBuilder(
+                  builder: (context, setState) {
+                    return Card(
+                      child: MouseRegion(
+                        onEnter: (_) => setState(() => isHovered = true),
+                        onExit: (_) => setState(() => isHovered = false),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Stack(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            ProductoDetalleScreen(
+                                                producto: producto),
+                                      ),
+                                    );
+                                  },
+                                  child: Image.network(
+                                    imageUrl,
+                                    height: 200,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return const Icon(
+                                          Icons.image_not_supported,
+                                          size: 200);
+                                    },
+                                  ),
+                                ),
+                                if (isHovered)
+                                  Positioned.fill(
+                                    child: Container(
+                                      color: Colors.black.withOpacity(0.5),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                            ListTile(
+                              title: Text(producto['nombre']),
+                              subtitle: FutureBuilder<String>(
+                                future: _calcularGanador(producto),
+                                builder: (context, ganadorSnapshot) {
+                                  if (ganadorSnapshot.connectionState ==
+                                      ConnectionState.waiting) {
+                                    return const Text("Calculando...");
+                                  } else if (ganadorSnapshot.hasError) {
+                                    return Text(
+                                        "Error: ${ganadorSnapshot.error}");
+                                  } else {
+                                    return Text(ganadorSnapshot.data ?? '');
+                                  }
+                                },
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ProductoDetalleScreen(
+                                        producto: producto),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
-                ),
-              ],
+                );
+              },
             ),
-          ),
-        );
-      },
-    );
-  },
-);
-
-
-
-          }
-        },
-      ),
     );
   }
 }
